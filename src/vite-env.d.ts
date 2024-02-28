3736a0595b92874efc1db2a0a57ca40302c7f30b
skyInWater <coderhy@qq.com>
2024-02-28 15:42:33 +0800

    build(工程化相关): 添加环境变量类型

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  // 在这里添加环境变量的类型
+  readonly VITE_APP_TITLE: string;
+
+  readonly VITE_APP_FLAG: string;
+  readonly VITE_APP_TARGET: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
