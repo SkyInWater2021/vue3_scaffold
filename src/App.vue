966b2f93c60c4de5ecf691673cf9fa7f08c4d56d
skyInWater <coderhy@qq.com>
2024-03-26 17:19:56 +0800

    chore(工程化相关): 解决自动整理import语句时,造成格式化两次的问题

--- src/App.vue
+++ src/App.vue
@@ -15,6 +15,7 @@ const documentHeight = () => {
   const doc = document.documentElement
   doc.style.setProperty("--doc-height", `${window.innerHeight}px`)
 }
+
 window.addEventListener("resize", documentHeight)
 documentHeight()
 
